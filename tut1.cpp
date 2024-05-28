d5e3391b06bdd35e7a1955d89beb2991f85624b0
kabir9382 <153837592+kabir9382@users.noreply.github.com>
2024-05-28 20:32:42 +0600

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+  if(int i = 0;i<5;i++){
+    cin>>i;
+
+  }
+  cout<<i<<endl;
+  
+  
+  
+   /*
+   int ac;
+   cin>>ac;
+   int a;
+   a = 9;
+
+   int b;
+   b = 6;
+
+   int c = a + b;
+   int d = a * b;
+
+  //c will add variable a and b
+   cout<<c<<endl;
+   
+   cout<<d<<endl;
+
+   int e = {(a*b)-(c*d)};
+   cout << e;
+*/
+  return 0;
+}
